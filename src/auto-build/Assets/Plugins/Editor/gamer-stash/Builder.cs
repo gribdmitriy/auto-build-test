4f54b3c1277dc4b3328a340a070f9dd6d4d7cc06
gribdmitriy <dmitrii.9rib@gmail.com>
2022-04-26 18:56:43 +0300

    semi-automatic  Android build from menu

---
+++ src/auto-build/Assets/Plugins/Editor/gamer-stash/Builder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Plugins.Editor
+{
+    public static class Builder
+    {
+        [MenuItem("Build/Build Android")]
+        public static void MyBuild()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = GetScenes(),
+                target = BuildTarget.Android,
+                locationPathName = "../../artifacts/build.apk"
+            };
+            
+            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            var summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                throw new Exception("Build failed");
+            }
+        }
+        
+        private static string[] GetScenes()
+        {
+            var scenes = new List<string>();
+            
+            for (var i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                if (EditorBuildSettings.scenes[i].enabled)
+                    scenes.Add(EditorBuildSettings.scenes[i].path);
+            }
+            
+            return scenes.ToArray();
+        }
+    }
+}
